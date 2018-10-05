ca6307d646cdcf78c85418ec13e1e5a4e6d3f525
Jeremy Holland <jeremy@jeremypholland.com>
2018-10-05 14:13:17 -0500

    Updating some libs

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject org.clojars.awebneck/lein-protoc "0.5.4"
+(defproject org.clojars.awebneck/lein-protoc "0.5.5"
   :description "Leiningen plugin for compiling Protocol Buffers"
   :url "https://github.com/awebneck/lein-protoc"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :eval-in-leiningen true
-  :profiles {:dev {:dependencies [[com.google.protobuf/protobuf-java "3.3.1"]]}}
-  :plugins [[lein-codox "0.10.3"]]
+  :profiles {:dev {:dependencies [[com.google.protobuf/protobuf-java "3.6.0"]]}}
+  :plugins [[lein-codox "0.10.4"]]
   :codox
   {:output-path "docs"
    :metadata {:doc/format :markdown}

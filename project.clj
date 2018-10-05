2549c9e2d74d1ebf79e33c16ef5c15cc30e04828
Jeremy Holland <jeremy@jeremypholland.com>
2018-10-05 11:27:15 -0500

    Adding core specs

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.awebneck/lein-protoc "0.5.2"
+(defproject org.clojars.awebneck/lein-protoc "0.5.3"
   :description "Leiningen plugin for compiling Protocol Buffers"
   :url "https://github.com/awebneck/lein-protoc"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,8 @@
   :min-lein-version "2.0.0"
   :eval-in-leiningen true
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/spec.alpha "0.2.176"]]
+                 [org.clojure/spec.alpha "0.2.176"]
+                 [org.clojure/core.specs.alpha "0.2.44"]]
   :profiles {:dev {:dependencies [[com.google.protobuf/protobuf-java "3.3.1"]]}}
   :plugins [[lein-codox "0.10.3"]]
   :codox
